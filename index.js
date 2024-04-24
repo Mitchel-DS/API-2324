a16772141e474fb693e31d679f8cc53928ba300e
Mitchel-DS <mitchel.staal@outlook.com>
2024-04-24 16:28:21 +0200

    minor changes

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+// dotenv
 dotenv.config();
 
 const app = express();
 app.use(express.urlencoded({ extended: true}));
 
+// ejs
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
+// server port setup
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Listening @ port ${PORT}`);
 });
 

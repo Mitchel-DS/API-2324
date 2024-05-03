677f583e903724ed4dd6cd8a6d1f1672e8330690
Mitchel-DS <mitchel.staal@outlook.com>
2024-05-03 02:26:44 +0200

    added some pages

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const app = express();
 import dotenv from 'dotenv';
 dotenv.config();
 
-
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
 
 app.get('/', async (req, res) => {
     try {
+        const getMovies = await fetchMovies();
+        console.log(getMovies);
         res.render('pages/index', { title: "Home - Webflix"}) 
     } catch (error) {
         console.log(error)
@@ -27,7 +28,13 @@ app.get('/', async (req, res) => {
  });
 
  // fetch movies
+const API_KEY = process.env.API_KEY;
 
+const fetchMovies = async () => {
+    const response = await fetch(`https://api.themoviedb.org/3/trending/all/week?language=en-US&api_key=${process.env.API_KEY}`);
+    const movies = await response.json();
+    return movies;
+}
 
 // server port setup
 const PORT = process.env.PORT || 3000;

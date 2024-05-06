7f7ff529ea7a8b40b82617ddebf85400a305c70d
Mitchel-DS <mitchel.staal@outlook.com>
2024-05-06 18:21:38 +0200

    added search function

--- index.js
+++ index.js
@@ -12,22 +12,35 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
 
-const fetchMovies = async () => {
+
+const fetchNowPlaying = async () => {
+    const response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1&api_key=${process.env.API_KEY}`);
+    const now_playing = await response.json();
+    return now_playing;
+}
+
+const fetchTrendingMovies = async () => {
     const response = await fetch(`https://api.themoviedb.org/3/trending/movie/week?language=en-US&api_key=${process.env.API_KEY}`);
     const movies = await response.json();
     return movies;
 }
 
-const fetchSeries = async () => {
+const fetchTrendingSeries = async () => {
     const response = await fetch(`https://api.themoviedb.org/3/trending/tv/week?language=en-US&api_key=${process.env.API_KEY}`);
     const series = await response.json();
     return series;
 }
 
+const fetchSearchResults = async (searchQuery) => {
+    const response = await fetch(`https://api.themoviedb.org/3/search/keyword?query=${searchQuery}&page=1&api_key=${process.env.API_KEY}`);
+    const searchResults = await response.json()
+    return searchResults;
+}
+
 app.get('/', async (req, res) => {
     try {
-        const getMovies = await fetchMovies();
-        const getSeries = await fetchSeries();
+        const getMovies = await fetchTrendingMovies();
+        const getSeries = await fetchTrendingSeries();
         res.render('pages/index', { title: "Home - Webflix", movies: getMovies.results, series: getSeries.results}) 
     } catch (error) {
         console.log(error)
@@ -48,6 +61,8 @@ app.get('/', async (req, res) => {
      try {
         const searchQuery = req.query.query;
         console.log(searchQuery);
+        const getSearchResults = await fetchSearchResults(searchQuery);
+        console.log(getSearchResults);
     } catch (error) {
         console.log(error)
     }

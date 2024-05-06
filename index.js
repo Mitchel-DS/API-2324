f66879e40008087ad9b6206ab571890766f3fb4f
Mitchel-DS <mitchel.staal@outlook.com>
2024-05-06 17:47:17 +0200

    updated 404 page

--- index.js
+++ index.js
@@ -12,12 +12,22 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
 
+const fetchMovies = async () => {
+    const response = await fetch(`https://api.themoviedb.org/3/trending/movie/week?language=en-US&api_key=${process.env.API_KEY}`);
+    const movies = await response.json();
+    return movies;
+}
+
+const fetchSeries = async () => {
+    const response = await fetch(`https://api.themoviedb.org/3/trending/tv/week?language=en-US&api_key=${process.env.API_KEY}`);
+    const series = await response.json();
+    return series;
+}
+
 app.get('/', async (req, res) => {
     try {
         const getMovies = await fetchMovies();
         const getSeries = await fetchSeries();
-        console.log(getMovies);
-        console.log(getSeries);
         res.render('pages/index', { title: "Home - Webflix", movies: getMovies.results, series: getSeries.results}) 
     } catch (error) {
         console.log(error)
@@ -34,6 +44,15 @@ app.get('/', async (req, res) => {
     }
  });
 
+ app.get('/search', async (req, res) => {
+     try {
+        const searchQuery = req.query.query;
+        console.log(searchQuery);
+    } catch (error) {
+        console.log(error)
+    }
+ });
+
  app.get('/*', async (req, res) => {
     try {
         res.render('pages/404', { title: "Webflix"}) 
@@ -41,18 +60,6 @@ app.get('/', async (req, res) => {
         console.log(error)
     }
  });
-
-const fetchMovies = async () => {
-    const response = await fetch(`https://api.themoviedb.org/3/trending/movie/week?language=en-US&api_key=${process.env.API_KEY}`);
-    const movies = await response.json();
-    return movies;
-}
-
-const fetchSeries = async () => {
-    const response = await fetch(`https://api.themoviedb.org/3/trending/tv/week?language=en-US&api_key=${process.env.API_KEY}`);
-    const series = await response.json();
-    return series;
-}
 
 // server port setup
 const PORT = process.env.PORT || 3000;

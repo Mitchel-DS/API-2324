7c8be8f3571c456bc5cbf4a0b06ebc64416a6e98
Mitchel-DS <mitchel.staal@outlook.com>
2024-05-07 07:56:20 +0200

    added attempt webanimation api

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,1 +1,17 @@
 console.log('script loaded');
+
+// Webanimation API
+const poster = document.querySelector('.moviePoster');
+poster.addEventListener('mouseover', function() {
+    let animation = poster.animate({
+        transform: ['scale(1)', 'scale(1.25)']
+    }, 300);
+    poster.style.transform = 'scale(1,25)';
+});
+
+poster.addEventListener('mouseout', function() {
+    let animation = poster.animate({
+        transform: ['scale(1.25)', 'scale(1)']
+    }, 300);
+    poster.style.transform = '';
+});
